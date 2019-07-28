58768f6ce2174e9c53d91c1fd2953d51a2ea323d
A Freilich <freilich.am@gmail.com>
2019-07-27 22:33:35 -0400

    work on setup for week 5 and start logic

--- main.js
+++ main.js
@@ -1,59 +1,68 @@
-const players = {
-    1: 'Aasha',
-    2: 'Amber',
-    3: 'Basit', 
-    4: 'Brandon',
-    5: 'Danny',
-    6: 'Jasmine',
-    7: 'Jenna',
-    8: 'Jonathan',
-    9: 'Justin',
-    10: 'Kai',
-    11: 'Kari', 
-    12: 'Kylie', 
-    13: 'Max',
-    14: 'Nour',
-    15: 'Paige',
-    16: 'Remy'
+const setup = require('./setupAfterWeek5')
+//console.log(JSON.stringify(setup, null, 2))
+
+const truthBooths = mirror(setup.truthBooths)
+
+function mirror (truthBooths) {
+    let result = {}
+    Object.keys(truthBooths).forEach(first => {
+	result[first] = result[first] || {}
+	Object.keys(truthBooths[first]).forEach(second => {
+	    result[second] = result[second] || {}
+	    result[first][second] = result[second][first] = truthBooths[first][second]
+	})
+    })
+    console.log(result)
+    return result
 }
 
-let numbers = {}
-Object.keys(players).forEach(key => { numbers[players[key]] = key} )
+const forbiddenMatches = getForbiddenFromTruthBoothsAndBlackouts(truthBooths, setup.ceremonies)
 
-const matchingString =
-`Aasha	Paige	Brandon	Max	Remy	Kai
-Amber	Nour	Nour	Paige	Nour	Nour
-Basit	Jonathan	Jonathan	Remy	Danny	Remy
-Brandon	Remy	Aasha	Jonathan	Jasmine	Max				
-Danny	Kai	Remy	Kai	Basit	Kari				
-Jasmine	Jenna	Justin	Nour	Brandon	Paige
-Jenna	Jasmine	Kai	Justin	Paige	Kylie
-Jonathan	Basit	Basit	Brandon	Kylie	Justin					
-Justin	Max	Jasmine	Jenna	Max	Jonathan					
-Kai	Danny	Jenna	Danny	Kari	Aasha					
-Kari	Kylie	Kylie	Kylie	Kai	Danny					
-Kylie	Kari	Kari	Kari	Jonathan	Jenna					
-Max	Justin	Paige	Aasha	Justin	Brandon					
-Nour	Amber	Amber	Jasmine	Amber	Amber					
-Paige	Aasha	Max	Amber	Jenna	Jasmine					
-Remy	Brandon	Danny	Basit	Aasha	Basit`
-
-let matchingObject = {}
-matchingString.split('\n').forEach(line => {
-    const names = line.split() // whitespace??
-    matchingObject[names[0]] = names.slice(0) // is this right??
+function getForbiddenFromTruthBoothsAndBlackouts (truthBooths, ceremonies) {
+    // todo: add
+    return truthBooths
 }
 
-const scores = [2, 2, 2, 1, 0]
+/*
+let argumentObject = {}
 
-// const truthBooths = 
+for (let i = 0; i < matchingCeremonies.length; i++) {
+    const ceremony = matchingCeremonies[i]
+    const liveCouples = getLiveCouples(ceremony)
+    const possibilities = getPossibilities(liveCouples, ceremony.freeBeams)
+    possibilities.forEach(x => tryPossibility(x, matchingCeremonies, forbiddenMatches, argumentObject))
+}
 
-/* steps:
-choose week with fewest 'free' matches,
-find most restricted person and choose a possible match,
-reduce number of free matches,
-recalculate other weeks 'free' matches if needed,
-if a week has no free matches add its restrictions
+// an argument object is a series of cases looking like:
+let argumentObject = [
+    {
+	name: 'Jonathan and Basit, Danny and Kai, in week 1;',
+	forbiddenMatches: {}, // a forbidden matches object
+	confirmedMatches: [], // an array of matches
+	ceremonyTotals: {}, // a ceremonies object
+	subCases: [] // an array of cases
+    }
+]
 
-repeat?
+function getLiveCouples (ceremony, forbiddenMatches) {
+    // go through the couples in the ceremony and filter based on forbidden matches
+}
+
+// a possibility is an array of matches
+function getPossibilities (liveCouples, numBeams) {
+    // this is a simple choose operation, choose all sets of size numBeams from liveCouples, return those arrays
+    
+}
+
+function tryPossibility (possibility, matchingCeremonies, forbiddenMatches, argumentObject) {
+    // add the rest of the couples from the ceremony to forbidden matches
+    // adjust the free beams in the remaining matching ceremonies
+    // add all of the couples in the possibility
+}
+
+function addCouple (couple, matchingCeremonies, forbiddenMatches) {
+    // remove all references to the couple from the matching ceremonies and forbiddenMatches
+}
+
+// console.log(JSON.stringify(argumentObject, null, 2))
 */
---
+++ setupAfterWeek5.js
@@ -0,0 +1,116 @@
+let playersObject = {
+    1: 'Aasha',
+    2: 'Amber',
+    3: 'Basit', 
+    4: 'Brandon',
+    5: 'Danny',
+    6: 'Jasmine',
+    7: 'Jenna',
+    8: 'Jonathan',
+    9: 'Justin',
+    10: 'Kai',
+    11: 'Kari', 
+    12: 'Kylie', 
+    13: 'Max',
+    14: 'Nour',
+    15: 'Paige',
+    16: 'Remy'
+}
+
+const players = Object.keys(playersObject).map(key => playersObject[key])
+
+// let numbers = {}
+// Object.keys(players).forEach(key => { numbers[players[key]] = key} )
+
+const matchingString = `Aasha	Paige	Brandon	Max	Remy	Kai
+Amber	Nour	Nour	Paige	Nour	Nour
+Basit	Jonathan	Jonathan	Remy	Danny	Remy
+Brandon	Remy	Aasha	Jonathan	Jasmine	Max				
+Danny	Kai	Remy	Kai	Basit	Kari				
+Jasmine	Jenna	Justin	Nour	Brandon	Paige
+Jenna	Jasmine	Kai	Justin	Paige	Kylie
+Jonathan	Basit	Basit	Brandon	Kylie	Justin					
+Justin	Max	Jasmine	Jenna	Max	Jonathan					
+Kai	Danny	Jenna	Danny	Kari	Aasha					
+Kari	Kylie	Kylie	Kylie	Kai	Danny					
+Kylie	Kari	Kari	Kari	Jonathan	Jenna					
+Max	Justin	Paige	Aasha	Justin	Brandon					
+Nour	Amber	Amber	Jasmine	Amber	Amber					
+Paige	Aasha	Max	Amber	Jenna	Jasmine					
+Remy	Brandon	Danny	Basit	Aasha	Basit`
+
+let matchingObject = {}
+const lines = matchingString.split('\n')
+for (let i = 0; i < lines.length; i++) {
+    const names = lines[i].split(/\s+/).filter(x => !!x)
+    for (let j = 1; j < names.length; j++) {
+	matchingObject[j] = matchingObject[j] || {}
+	matchingObject[j][names[0]] = names[j]
+    }
+}
+
+// const scores = [2, 2, 2, 1, 0]
+
+/* steps:
+choose week with fewest 'free' matches,
+choose one of the matches in that week (the one appearing in the most rounds?) and reduce the number of free matches for each round it appears in,
+if that was its last match, add its restrictions,
+if at some point someone has no possible matches, abort
+repeat,
+*/
+
+const ceremonies = [
+    {
+	week: 1,
+	matching: matchingObject[1],
+	beams: 2,
+	freeBeams: 2
+    },
+    {
+	week: 2,
+	matching: matchingObject[2],
+	beams: 2,
+	freeBeams: 2
+    },
+    {
+	week: 3,
+	matching: matchingObject[3],
+	beams: 2,
+	freeBeams: 2
+    },
+    {
+	week: 4,
+	matching: matchingObject[4],
+	beams: 1,
+	freeBeams: 1
+    },
+    {
+	week: 5,
+	matching: matchingObject[5],
+	beams: 0,
+	freeBeams: 0
+    }
+]
+
+
+/*
+Justin & Nour	1	Not A Match
+Brandon & Remy	2	Not A Match
+Jenna & Kai	3	Not A Match
+Danny & Jenna	4	Not A Match
+Kari & Kylie	5	Not A Match
+*/
+const truthBooths = {
+    Justin: {Nour: false},
+    Brandon: {Remy: false},
+    Kai: {Jenna: false},
+    Danny: {Jenna: false},
+    Kari: {Kylie: false}
+}
+
+module.exports = {
+    truthBooths: truthBooths,
+    ceremonies: ceremonies,
+    players: players
+//    numbers: numbers
+}
