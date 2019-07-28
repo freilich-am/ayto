691b4edb8e1419ea473a643995833ddc99253a8d
A Freilich <freilich.am@gmail.com>
2019-07-28 05:23:31 -0400

    Generate possibilities list

--- main.js
+++ main.js
@@ -32,13 +32,14 @@ function getForbiddenFromTruthBoothsAndBlackouts (truthBooths, ceremonies) {
 }
 
 
-const ceremonies = calculateLiveMatchings(setup.ceremonies, forbiddenMatches, [["Basit", "Jonathan"], ['Danny', 'Kai']])
+const ceremonies = calculateLiveMatchings(setup.ceremonies, forbiddenMatches, [["Basit", "Jonathan"]])
 
 // console.log(JSON.stringify(ceremonies, null, 2))
 
 function calculateLiveMatchings (ceremonies, forbiddenMatches, matchings) {
   //    console.log(JSON.stringify(ceremonies))
-  return ceremonies.map(x => findLiveMatches(x, forbiddenMatches, matchings))
+  let newCeremonies = JSON.parse(JSON.stringify(ceremonies))
+  return newCeremonies.map(x => findLiveMatches(x, forbiddenMatches, matchings))
 }
 
 // todo: incorporate initial matchings into initialization
@@ -79,17 +80,48 @@ const unresolvedCeremonies = ceremonies.filter(ceremony => ceremony.freeBeams)
 
 let argumentObject = [
   {
-    name: 'Week 5 (Jonathan and Basit, Danny and Kai)', // always put in alphabetical order?
+    name: 'Week 5 (Jonathan and Basit)', // always put in alphabetical order?
     forbidden: forbiddenMatches,
     ceremonies: unresolvedCeremonies,
-    matchings: [["Basit", "Jonathan"], ['Danny', 'Kai']],
-    cases: expandCases(forbiddenMatches, unresolvedCeremonies,[["Basit", "Jonathan"], ['Danny', 'Kai']], 20),
+    matchings: [["Basit", "Jonathan"]],
+    cases: expandCases(forbiddenMatches, unresolvedCeremonies,[["Basit", "Jonathan"]], 20),
     notes: ''
   }
 ]
 
+argumentObject = argumentObject.map(prune)
+
+function prune (argObj) {
+  argObj = propogateFailures(argObj)
+  argObj = removeFailures(argObj)
+  return argObj
+}
+
+function propogateFailures (poss) {
+  if (poss.cases) {
+    propogateFailures(poss.cases)
+  }
+  poss.failure = poss.failure || (poss.cases && poss.cases.every(p => p.failure))
+  return poss
+}
+
+function removeFailures (argObj) {
+  if (argObj.cases) {
+    argObj.cases = argObj.cases.map(removeFailures)
+    argObj.cases = argObj.cases.filter(poss => !poss.failure)
+  }
+  return argObj
+}
+
 // console.log(JSON.stringify(argumentObject, null, 2))
-printArgumentObject(argumentObject)
+// printArgumentObject(argumentObject)
+let newArr = []
+printArgumentObject2(argumentObject, newArr)
+for (let i = 0; i < newArr.length; i++) {
+  newArr[i].possNum = i
+}
+console.log(JSON.stringify(newArr, null, 2))
+
 
 // todo: print function for forbidden
 // todo: print function for matchings
@@ -97,17 +129,48 @@ printArgumentObject(argumentObject)
 function printArgumentObject (argumentObject, nestingLevel = 0, prefix = '') {
   argumentObject.forEach(currCase => {
     let lines = []
-    if (currCase.name) lines.push('The case we are considering is ' + prefix + '; ' + currCase.name)
+    if (currCase.name) lines.push('The case we are considering is ' + prefix + `${prefix ? '; ' : ''}` + currCase.name)
     const matchesString = currCase.matchings ? stringify(currCase.matchings) : ''
     if (matchesString) lines.push('So far we have matched up: ' + matchesString)
     // todo: some stuff about the ceremonies and state of forbidden matches?
     if (currCase.notes) lines.push('Notes: ' + currCase.notes)
+    if (currCase.failure) lines.push('Failure')
     //	console.log(lines)
     lines.forEach(line => {
       console.log('*' + Array(nestingLevel + 1).join(' ') + line)
     })
-    if (currCase.cases) printArgumentObject(currCase.cases, nestingLevel + 1, prefix + `; ${currCase.name}`)
+    if (currCase.cases) printArgumentObject(currCase.cases, nestingLevel + 1, prefix + `${prefix ? '; ' : ''}${currCase.name}`)
   })
+}
+
+function printArgumentObject2 (argumentObject, newArr) {
+  argumentObject.forEach(currCase => {
+    if (currCase.cases && currCase.cases.length) {
+      printArgumentObject2(currCase.cases, newArr)
+    } else {
+      // const matchesString = currCase.matchings ? stringify(currCase.matchings) : ''
+      newArr.push(formatCase(currCase))
+    }
+  })
+}
+
+function formatCase (poss) {
+  const matchedPeople = [].concat.apply([], poss.matchings)
+  const unmatchedPeople = setup.players.filter(player => {
+    return !matchedPeople.includes(player)
+  })
+  let newForbidden = {}
+  Object.keys(poss.forbidden)
+    .filter(key => unmatchedPeople.includes(key))
+    .forEach(key => {
+      newForbidden[key] = {}
+      Object.keys(poss.forbidden[key])
+        .filter(k => unmatchedPeople.includes(k))
+        .forEach(k => {
+          newForbidden[key][k] = poss.forbidden[key][k]
+        })
+    })
+  return {unmatchedPeople, forbidden: newForbidden, matchings: poss.matchings}
 }
 
 // you might not need to put all matchings into this, only new ones
@@ -116,17 +179,17 @@ function expandCases (forbidden, ceremonies, matchings, recursive = false) {
   if (!liveCeremonies.length) {
     //	console.log('input to expland cases')
     //	console.log(JSON.stringify({forbidden, ceremonies, matchings, recursive}, null, 2))
-    return []
+    return [] // [{notes: 'no more ceremonies'}] // + JSON.stringify({forbidden, ceremonies, matchings})}]
   }
   // if none available, terminate
   const possibilities = getPossibilities(ceremonies[0]) // change
   if (!possibilities.length) {
-    return [{notes: 'this sucks'}]
+    return [{notes: 'this sucks', failure: true}]
   }
   return possibilities.map(pos => {
     const tryResult = tryPos(pos, forbidden, ceremonies, matchings)
     const newMatchings = matchings.concat(pos)
-    if (!recursive) {console.log(JSON.stringify('ruh roh'))}
+    // if (!recursive) {console.log(JSON.stringify('ruh roh'))}
     return {
       name: stringify(pos), // special stringify
       forbidden: tryResult.forbidden,
@@ -168,22 +231,22 @@ function getPossibilities (ceremony) {
 }
 
 function tryPos (pos, forbidden, ceremonies, matchings) {
-  //  let noteLines = []
-  let newForbidden = forbidden // a copy please!
+  let noteLines = []
+  let newForbidden = JSON.parse(JSON.stringify(forbidden)) // a copy please!
   // first, incorporate matches, removing all references etc
   ceremonies = calculateLiveMatchings(ceremonies, forbidden, pos)
   // second, update ceremonies and remove resolved
   // add the rest of the ceremony's matches to forbidden (todo)
   ceremonies.filter(ceremony => !ceremony.freeBeams).forEach(x => {
-    //    noteLines.push(`week ${x.week} has no remaining free beams, here's the whole object ${JSON.stringify(x, null, 2)}`)
+    noteLines.push(`week ${x.week} has no remaining free beams`) //, here's the whole object ${JSON.stringify(x, null, 2)}`)
     x.liveMatches.forEach(x => {
       newForbidden[x[0]][x[1]] = newForbidden[x[1]][x[0]] = true
     })
   })
   // third find an available ceremony and choose its matches
   let newCeremonies = ceremonies.filter(ceremony => ceremony.freeBeams) // i hope this makes a copy!
-  const cerm = newCeremonies.shift()
-  return {ceremonies: newCeremonies, forbidden: newForbidden}
+//  const cerm = newCeremonies.shift()
+  return {ceremonies: newCeremonies, forbidden: newForbidden, notes: noteLines.join('; ')}
 }
 
 
